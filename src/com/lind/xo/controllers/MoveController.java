72b693f3228cf13852da473306a83e3a56534c4d
Lindelor <leontev150@mail.ru>
2020-07-15 13:10:21 +0400

    MoveController created

--- src/com/lind/xo/controllers/MoveController.java
+++ src/com/lind/xo/controllers/MoveController.java
@@ -1,14 +1,20 @@
 package com.lind.xo.controllers;
 
 import com.lind.xo.model.Field;
+import com.lind.xo.model.Figure;
+import com.lind.xo.model.exceptions.AlreadyOccupiedException;
+import com.lind.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveController {
 
-    public void applyFigure(final Field field, final Point point) {
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupiedException,
+                                                        InvalidPointException {
 
-
+        field.setFigure(point, figure);
 
     }
 
