e59e209935c5fc6d74cd5c44d41f18b8446f7e55
Lindelor <leontev150@mail.ru>
2020-07-15 14:46:10 +0400

    CurrentMoveController done

--- src/com/lind/xo/controllers/CurrentMoveController.java
+++ src/com/lind/xo/controllers/CurrentMoveController.java
@@ -1,4 +1,40 @@
 package com.lind.xo.controllers;
 
+import com.lind.xo.model.Field;
+import com.lind.xo.model.Figure;
+import com.lind.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+import static com.lind.xo.model.Figure.X;
+import static com.lind.xo.model.Figure.O;
+
 public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+
+        try {
+            return countOfFigures(field);
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Figure countOfFigures(final Field field) throws InvalidPointException {
+        int countX = 0;
+        int countO = 0;
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < field.getSize(); j++) {
+                if (field.getFigure(new Point(i, j)) != null) {
+                    if (field.getFigure(new Point(i, j)) == X) countX++;
+                    else countO++;
+                }
+            }
+        }
+        if (countO + countX == 9) return null;
+        if (countO == countX) return X;
+        else return O;
+    }
+
 }
